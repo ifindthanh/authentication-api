8cc937e0ff88363e959002faa91eb036b7425061
tts <sontt246@gmail.com>
2018-02-08 16:23:16 +0700

    Test

---
+++ src/main/java/com/google/oauthsample/Constants.java
@@ -0,0 +1,6 @@
+package com.google.oauthsample;
+
+public class Constants {
+    public static final String CLIENT_ID = "763370393822-bo5e6ha89qugrv7t4qghst6ip158rqcf.apps.googleusercontent.com";
+    public static final String CLIENT_SECRET = "vlXH9mq8qEF_rLTr_M1-mECA";
+}
